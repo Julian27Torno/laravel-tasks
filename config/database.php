14dbff5a600b3390958362c2e21d83c84f20dbf1
Julian Carlos Torno <118442927+Julian27Torno@users.noreply.github.com>
2025-10-15 10:06:44 +0000

    fixed issue

--- config/database.php
+++ config/database.php
@@ -44,24 +44,24 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('AZURE_MYSQL_HOST', '127.0.0.1'),
-            'port' => env('AZURE_MYSQL_PORT', '3306'),
-            'database' => env('AZURE_MYSQL_DBNAME', 'forge'),
-            'username' => env('AZURE_MYSQL_USERNAME', 'forge'),
-            'password' => env('AZURE_MYSQL_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', base_path('/home/site/wwwroot/ssl/DigiCertGlobalRootCA.crt.pem')),
-            ]) : [],
-        ],
+    'driver' => 'mysql',
+    'host' => env('AZURE_MYSQL_HOST', '127.0.0.1'),
+    'port' => env('AZURE_MYSQL_PORT', '3306'),
+    'database' => env('AZURE_MYSQL_DBNAME', 'forge'),
+    'username' => env('AZURE_MYSQL_USERNAME', 'forge'),
+    'password' => env('AZURE_MYSQL_PASSWORD', ''),
+    'unix_socket' => env('DB_SOCKET', ''),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'strict' => true,
+    'engine' => null,
+    'options' => extension_loaded('pdo_mysql') ? array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/home/site/wwwroot/ssl/DigiCertGlobalRootCA.crt.pem'),
+    ]) : [],
+],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
